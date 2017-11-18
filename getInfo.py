30c2e1bb5d94876cdba8f18f27173ba1c716e34e
fkoto <foivos.kot@gmail.com>
2017-11-18 22:33:37 +0200

    Added parser. Integrated parser with getInfo

--- getInfo.py
+++ getInfo.py
@@ -1,1 +1,19 @@
 import sorter
+import mapper
+import clusterClasses
+import parser
+import sys
+
+parser = parser.Parser()
+myParser = parser.getParser()
+
+args = myParser.parse_args()
+
+if len(sys.argv) == 1:
+    print myParser.print_help()
+    exit(1)
+else:
+    print args
+
+
+
---
+++ parser.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+
+class Parser():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='Generating a mapping for a mpi proccess.')
+        self.parser.add_argument('-host', '-H', '--host', nargs='*', help='Hosts on which to run application')
+        self.parser.add_argument('-hostfile', '--hostfile', '-machinefile', '--machinefile',
+                                 type=argparse.FileType('r'), help='File containing information on the nodes')
+        self.parser.add_argument('-c', '-n', '--n', '-np', dest='procs', type=int, help='Number of processes to launch')
+        self.parser.add_argument('--map-by', default='node', type=str, help='Mapping of processes.')
+        self.parser.add_argument('-nooversubscribe', '--nooversubscribe', action='store_true'
+                                 , help='Do not oversubscribe any nodes')
+        self.parser.add_argument('-bynode', '--bynode', action='store_true'
+                                 , help='assign processes one per node, cycling in a RR fashion')
+        self.parser.add_argument('-rank-by', default='slot', choices=['slot', 'core', 'node']
+                                 , type=str, help='Ranking of processes.')
+        self.parser.add_argument('-rf', '--rankfile', type=argparse.FileType('r'), help='Provide a ranking file')
+        self.parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+                                 help='Where final result will be printed. (default: stdout)')
+        self.parser.add_argument('args', nargs=argparse.REMAINDER)  # gather rest of items in a list
+
+    def getParser(self):
+        return self.parser
