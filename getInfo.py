93c852f5a77a27b3e36db2aa0ba7c275f98485c0
fkoto <foivos.kot@gmail.com>
2017-11-19 21:56:14 +0200

    Fully functional getInfo. minor mods to parser

--- getInfo.py
+++ getInfo.py
@@ -16,4 +16,87 @@ else:
     print args
 
 
+if args.rankfile is not None:
+    print 'rank file mode is not yet functional! Exiting'
+    exit(1)
+
+if args.host is not None:
+    print 'host arg is specified!'
+    temp1 = []
+    temp2 = []
+    for el in args.host:
+        if (',' in el):
+            temp1.extend(el.split(','))
+        else:
+            temp2.append(el)
+
+    args.host = temp2
+    args.host.extend(temp1)
+
+#get cluster parameters
+myCluster = clusterClasses.Cluster()
+
+if args.hostfile is not None:
+    print 'hostfile is specified!'
+    myCluster.getNodesFromHostFile(args.hostfile, args.cores)
+    myCluster.printClusterDetails()
+
+    if args.host is not None:
+        print 'also host is specified!'
+        ind = []
+        for el in myCluster.nodes:
+            if el.id in args.host:
+                ind.append(el)
+
+        myCluster.nodes = ind
+        print 'final cluster'
+        myCluster.printClusterDetails()
+elif args.hostfile is None and args.host is not None:
+    numOfSlots = input('Please specify number of slots on each host:')
+
+    for host in args.host:
+        nod = clusterClasses.Node(host, numOfSlots=numOfSlots, numOfCoresPerSlot=args.cores)
+        myCluster.nodes.append(nod)
+else:
+    #hostfile is None and host is None
+    numOfNodes = input('Please specify number of host on the cluster:')
+    numOfSlots = input('Please specify number of slots on each host:')
+
+    for i in range(numOfNodes):
+        nod = clusterClasses.Node('node' + str(i), numOfSlots=numOfSlots, numOfCoresPerSlot=args.cores)
+
+
+#Get mapping and ranking parameters
+if args.bynode is True:
+    print 'By node!'
+    mapmode = 'node'
+    rankmode = 'node'
+    ppr = 1
+else:
+    print 'map-by ' + args.map_by
+    print 'rank-by ' + args.rank_by
+
+    if ':' in args.map_by:
+        split = args.map_by.split(':')
+        ppr = split[1]
+        mapmode = split[-1]
+    else:
+        mapmode = args.map_by
+        ppr = 1
+
+    rankmode = args.rank_by
+
+
+myMapper = mapper.Mapper(mapmode)
+myRanker = sorter.Sorter(rankmode)
+
+print 'Initiating ' + str(args.procs) + ' processes'
+mapIds = myMapper.doMapping(myCluster, args.procs, ppr)
+
+finalIds = myRanker.compare(mapIds)
+
+cnt = 0
+for el in finalIds:
+    args.outfile.write(str(cnt) + ' ' + el +'\n')
+    cnt += 1
 
--- parser.py
+++ parser.py
@@ -4,16 +4,18 @@ import sys
 class Parser():
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Generating a mapping for a mpi proccess.')
+        self.parser.add_argument('-core', '--core', dest = 'cores', type=int, default=1,
+                                 help='Number of cores per slot')
         self.parser.add_argument('-host', '-H', '--host', nargs='*', help='Hosts on which to run application')
         self.parser.add_argument('-hostfile', '--hostfile', '-machinefile', '--machinefile',
                                  type=argparse.FileType('r'), help='File containing information on the nodes')
-        self.parser.add_argument('-c', '-n', '--n', '-np', dest='procs', type=int, help='Number of processes to launch')
-        self.parser.add_argument('--map-by', default='node', type=str, help='Mapping of processes.')
+        self.parser.add_argument('-c', '-n', '--n', '-np', dest='procs', type=int, default=1, help='Number of processes to launch')
+        self.parser.add_argument('-map-by', '--map-by', default='node', type=str, help='Mapping of processes.')
         self.parser.add_argument('-nooversubscribe', '--nooversubscribe', action='store_true'
                                  , help='Do not oversubscribe any nodes')
         self.parser.add_argument('-bynode', '--bynode', action='store_true'
                                  , help='assign processes one per node, cycling in a RR fashion')
-        self.parser.add_argument('-rank-by', default='slot', choices=['slot', 'core', 'node']
+        self.parser.add_argument('-rank-by', '--rank-by', default='slot', choices=['slot', 'core', 'node']
                                  , type=str, help='Ranking of processes.')
         self.parser.add_argument('-rf', '--rankfile', type=argparse.FileType('r'), help='Provide a ranking file')
         self.parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
